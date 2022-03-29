5966e9d620992c647a67f6f220cd1fb3ef69bd75
Eric Malone <ejmalone@gmail.com>
2022-03-29 10:55:42 -0700

    validate engine attributes & some method annotations

--- app/models/engine.rb
+++ app/models/engine.rb
@@ -23,13 +23,23 @@
 #  user_id  (user_id => users.id)
 #
 class Engine < ApplicationRecord
+  CYLINDER_OPTS = [ 1, 2, 4 ].freeze
+  VALVES_PER_CYLINDER_OPTS = [ 2, 4 ].freeze
+
   belongs_to :user
   has_many :cylinders, dependent: :destroy
 
   after_create :create_internals
 
-  private
+  validates :num_cylinders, inclusion: CYLINDER_OPTS
+  validates :valves_per_cylinder, inclusion: VALVES_PER_CYLINDER_OPTS
+  validate :valve_clearances_are_valid
 
+  # --------------------------------------------------------------
+  private
+  # --------------------------------------------------------------
+
+  # --------------------------------------------------------------
   def create_internals
     num_cylinders.times do |cyl_index|
       cylinders << Cylinder.create(cylinder_num: cyl_index + 1) do |cylinder|
@@ -39,6 +49,22 @@ class Engine < ApplicationRecord
           cylinder.valves << Valve.create(intake_or_exhaust: Valve::EXHAUST, valve_num: (valve_num += 1))
         end
       end
+    end
+  end
+
+  # --------------------------------------------------------------
+  def valve_clearances_are_valid
+    clearances = [
+      %w[ intake_min intake_max ],
+      %w[ exhaust_min exhaust_max ]
+    ]
+
+    clearances.flatten.each do |clearance|
+      errors.add(clearance, "can't be less than zero") if send(clearance) <= 0.0
+    end
+
+    clearances.each do |(min,max)|
+      errors.add(:base, "#{ min.humanize } and #{ max.humanize } is an invalid range") if send(min) >= send(max)
     end
   end
 end
--- test/models/engine_test.rb
+++ test/models/engine_test.rb
@@ -25,16 +25,19 @@
 require "test_helper"
 
 class EngineTest < ActiveSupport::TestCase
+  # --------------------------------------------------------------
   setup do
     @cylinders = 2
     @valves_per_cylinder = 2
     @engine = Engine.create(user: users(:one), num_cylinders: @cylinders, valves_per_cylinder: @valves_per_cylinder)
   end
 
+  # --------------------------------------------------------------
   test "engine has appropriate number of cylinders" do
     assert_equal @engine.cylinders.count, @cylinders
   end
 
+  # --------------------------------------------------------------
   test "engine has appropriate number of valves" do
     assert_equal @engine.cylinders.map(&:valves).flatten.count, @valves_per_cylinder * 2
   end
