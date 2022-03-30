2cee64b2524ef47e5a8aa1d0164c64fde980ea58
Eric Malone <ejmalone@gmail.com>
2022-03-29 17:09:20 -0700

    create valves in better order (in, in, ex, ex)

--- app/models/engine.rb
+++ app/models/engine.rb
@@ -52,10 +52,11 @@ class Engine < ApplicationRecord
   def create_internals
     num_cylinders.times do |cyl_index|
       cylinders << Cylinder.create(cylinder_num: cyl_index + 1) do |cylinder|
-        valve_num = 0
-        (valves_per_cylinder / 2).times do
-          cylinder.valves << Valve.create(intake_or_exhaust: Valve::INTAKE, valve_num: (valve_num += 1))
-          cylinder.valves << Valve.create(intake_or_exhaust: Valve::EXHAUST, valve_num: (valve_num += 1))
+        valves_per_cylinder.times do |i|
+          cylinder.valves << Valve.create(
+            intake_or_exhaust: i < valves_per_cylinder / 2 ? Valve::INTAKE : Valve::EXHAUST,
+            valve_num: i + 1
+          )
         end
       end
     end
