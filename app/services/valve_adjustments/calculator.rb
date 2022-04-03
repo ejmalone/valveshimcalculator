53e655dc22c4f41fb0e628e72461b98fab6f40cf
Eric Malone <ejmalone@gmail.com>
2022-04-03 09:50:24 -0700

    showing new shim value

--- app/services/valve_adjustments/calculator.rb
+++ app/services/valve_adjustments/calculator.rb
@@ -8,6 +8,16 @@ module ValveAdjustments
     end
 
     # --------------------------------------------------------------
+    def new_shim_thickness(valve)
+      if well_in_spec?(valve)
+        ' n/a '
+      else
+        # TODO: round to nearest 5
+        (valve.gap - max_gap(valve) + valve.shim.thickness.to_d / 100) * 100
+      end
+    end
+
+    # --------------------------------------------------------------
     def valve_style(valve)
       if well_in_spec?(valve)
         'green'
@@ -18,6 +28,11 @@ module ValveAdjustments
       else
         'grey'
       end
+    end
+
+    # --------------------------------------------------------------
+    def max_gap(valve)
+      valve_range(valve).last
     end
 
     # --------------------------------------------------------------
