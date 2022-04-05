97ee7eed21f78336bb8f20469bb0083490de375f
Eric Malone <ejmalone@gmail.com>
2022-04-05 16:35:09 -0700

    missed a bug in creating all shims for an engine

--- app/services/shims/shim_creator.rb
+++ app/services/shims/shim_creator.rb
@@ -14,8 +14,8 @@ module Shims
     # --------------------------------------------------------------
     def create
       @parameters.each do |valve_id, _|
-        valve = Valve.joins(cylinder: :engine).where(valves: { id: valve_id.to_i }, engines: { user_id: user.id }).last
-        Shim.create_for_valve!(valve, @parameters[valve_id][:thickness].to_i)
+        valve = Valve.joins(cylinder: :engine).where(valves: { id: valve_id.to_i }, engines: { user_id: @user.id }).last
+        Shim.create_for_engine!(valve, @parameters[valve_id][:thickness].to_i)
         valve.update(gap: @parameters[valve_id][:gap].to_d)
       end
     end
