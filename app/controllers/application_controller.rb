95460043dbc1e108177f6152b527e5b65f2d7608
Eric Malone <ejmalone@gmail.com>
2022-04-07 18:31:38 -0700

    debug helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@
 
 class ApplicationController < ActionController::Base
   def index; end
+
+  # --------------------------------------------------------------
+  def enable_debug
+    user_session["debug"] = true
+    redirect_to :root
+  end
+
+  # --------------------------------------------------------------
+  def disable_debug
+    user_session.delete("debug")
+    redirect_to :root
+  end
 end
