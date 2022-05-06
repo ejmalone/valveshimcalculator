03b79ccdf360f2115a50e0771a0cc4f631303295
Eric Malone <ejmalone@gmail.com>
2022-05-06 14:42:31 -0700

    all in one deploy to ecs

---
+++ aws/deploy_site.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex 
+
+docker build -t "motorcycleshims/web:production" -f docker/Dockerfile.production --secret id=master_key,src=config/credentials/production.key .
+
+aws ecr get-login-password --region us-west-2 --profile app_runner | docker login --username AWS --password-stdin 952598771041.dkr.ecr.us-west-2.amazonaws.com
+
+docker tag motorcycleshims/web:production 952598771041.dkr.ecr.us-west-2.amazonaws.com/motorcycleshims/web:production
+
+docker push 952598771041.dkr.ecr.us-west-2.amazonaws.com/motorcycleshims/web:production
+
+aws --profile app_runner ecs register-task-definition --cli-input-json file://$PWD/aws/ecs_task.json
+
+aws --profile app_runner ecs update-service --cluster "shims-cluster" --service "shimsweb" --task-definition "shimsweb"
