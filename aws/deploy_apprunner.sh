d227d8b86efe71bfdc33cba6f206688ee836d67d
Eric Malone <ejmalone@gmail.com>
2022-05-19 13:40:47 -0700

    finally got apprunner working with rds
    Opened the RDS subnet to 10.0.0.0/16, assigned App Runner to the
    RDS subnets, and using a security group with access to Postgres and
    HTTPS (for Secrets Manager).

--- aws/deploy_apprunner.sh
+++ aws/deploy_apprunner.sh
@@ -2,4 +2,7 @@
 
 set -ex
 
-aws --profile app_runner apprunner create-service --cli-input-json file://apprunner.json
+# create a vpc connection that is on the db subnets and has security group access to the db & secrets
+# aws --profile app_runner apprunner create-vpc-connector --vpc-connector-name shims-connector --subnets subnet-01a96a99c406ad526 subnet-0f055b5760cbe0703 --security-groups sg-0b5e3654beb38ed72
+
+aws --profile app_runner apprunner create-service --cli-input-json file://aws/apprunner.json
