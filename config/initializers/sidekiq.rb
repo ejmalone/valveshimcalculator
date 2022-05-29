945636d213529c29e16bd62470a9e0751ce0b5f7
Eric Malone <ejmalone@gmail.com>
2022-05-28 17:52:54 -0700

    specify sidekiq queue, log to file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,7 @@ sidekiq_config = { url: ENV['REDIS_URL'] }
 
 Sidekiq.configure_server do |config|
   config.redis = sidekiq_config
+  config.error_handlers << proc {|ex,ctx_hash| Rollbar.error(ex, context_hash: ctx_hash) }
 end
 
 Sidekiq.configure_client do |config|
