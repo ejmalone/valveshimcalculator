8f5f6779c1fb91217cf2c6aad7a17bf99b0b5c4b
Eric Malone <ejmalone@gmail.com>
2022-12-23 09:26:56 -0800

    bit of adjustment ux cleanup
    don't show adjustment state on index page

--- app/helpers/valve_adjustments_helper.rb
+++ app/helpers/valve_adjustments_helper.rb
@@ -5,6 +5,10 @@ module ValveAdjustmentsHelper
     "#{mileage / 1000}k mile"
   end
 
+  def completion_date_mileage(valve_adjustment)
+    "#{ valve_adjustment.adjustment_date.to_fs(:rfc822) } (#{ number_with_delimiter(valve_adjustment.mileage) } miles)"
+  end
+
   def valve_adjust_attrs(status)
     classname, title = case status
                        when ValveAdjustment::IN_SPEC then ['in-spec', 'Well within threshold']
